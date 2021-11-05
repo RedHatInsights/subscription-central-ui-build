08b2514c53cc0d8d964b4d3410f46de7f881f770
Travis CI User <travis@example.org>
2021-11-05 19:49:49 +0000

    Teamnado-2546 Update to v2 APIs (#44)
    * Fix dev webpack config
    
    * Update user and replace manifests endpoint
    
    * Update remaining endpoints to v2
    
    * Replace isOrgAdmin with canWriteManifests
    
    * Temporarily remove manifest list kebab
    
    * Hide delete if no write permission
    
    * Increase test coverage to required thresholds
    
    * Prevent premature API requests for side panel
    
    * Add missing dev dependencies
    
    * Update snapshots
    
    * Run npm install
    
    * Add semver
    
    * Use semver to check versions for SCA status
    
    * use npm version 7.21.0
    
    Co-authored-by: John Buscemi <jbuscemi@redhat.com>
    Co-authored-by: John Buscemi <11836261+jmbuscemi@users.noreply.github.com>

---
+++ js/915.6b05a95b9ea9000eb6d7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksubscription_central=self.webpackChunksubscription_central||[]).push([[915],{53915:(e,n,t)=>{t.r(n),t.d(n,{default:()=>u});var s=t(92950),i=t.n(s),c=t(334),l=t(86350),r=t(27839);const u=(0,c.withRouter)((function(){return(0,s.useEffect)((function(){var e,n,t;null===(e=insights)||void 0===e||null===(n=e.chrome)||void 0===n||null===(t=n.appAction)||void 0===t||t.call(n,"no-permissions")}),[]),i().createElement(l.Z,null,i().createElement(r.Z,{serviceName:"Manifests"}))}))}}]);
+//# sourceMappingURL=../sourcemaps/915.32cc671861589a02f519.js.map
