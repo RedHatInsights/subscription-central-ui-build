bf49e2d7b937c0a3aa48862d7e9ccdfe851b75f5
Travis CI User <travis@example.org>
2021-11-29 15:21:39 +0000

    TEAMNADO-3068 - Add learn more link to manifests (#47)
    * Add learn more link to manifests
    
    * Update jest snapshots
    
    Co-authored-by: Dmitrii Malyshev <dmalyshe@dmalyshe.remote.csb>

--- js/412.df6a7327ca0d8c780d8d.js
+++ js/412.df6a7327ca0d8c780d8d.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksubscription_central=self.webpackChunksubscription_central||[]).push([[412],{28412:(e,t,r)=>{r.r(t),r.d(t,{default:()=>i});var n=r(92950),a=r.n(n),c=r(334),o=r(43476),s=r(30319),l=r(30050),u=r(35240);const i=function(e){var t=e.logger;return a().createElement(o.Provider,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},a().createElement(c.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},a().createElement(l.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.1a570497c8679b31488f.js.map
+//# sourceMappingURL=../sourcemaps/412.5e044b53d0dba542ac65.js.map
