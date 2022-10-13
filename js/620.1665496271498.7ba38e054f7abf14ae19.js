dde834e2feae049f8b83963748691a9c0091f525
Travis CI User <travis@example.org>
2022-10-13 18:47:49 +0000

    TEAMNADO 4265 merge to main  (#135)

--- js/620.1665496271498.7ba38e054f7abf14ae19.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscription_central=self.webpackChunksubscription_central||[]).push([[620],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var r=n(2950),a=n.n(r),c=n(334),o=n(6706),s=n(319),l=n(50),u=n(5240);const i=function(e){var t=e.logger;return a().createElement(o.zt,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},a().createElement(c.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},a().createElement(l.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/620.d0e66744bb534943f96e2dadd92d4a6f.js.map
