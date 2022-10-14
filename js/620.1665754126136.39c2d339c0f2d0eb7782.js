60f83d42551dc46dbe08babc499ef58230a536f2
Travis CI User <travis@example.org>
2022-10-14 13:30:20 +0000

    Request to merge TEAMNADO-4265 to "main-stable" (#136)

---
+++ js/620.1665754126136.39c2d339c0f2d0eb7782.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksubscription_central=self.webpackChunksubscription_central||[]).push([[620],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var r=n(2950),a=n.n(r),c=n(334),o=n(6706),s=n(319),l=n(50),u=n(5240);const i=function(e){var t=e.logger;return a().createElement(o.zt,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},a().createElement(c.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},a().createElement(l.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/620.56cf27f3c3d6613c473f99b5a0ac635f.js.map
