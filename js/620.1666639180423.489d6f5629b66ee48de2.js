82389fef3b4d36b45f22ce1654d087b0eb6b571c
Travis CI User <travis@example.org>
2022-11-03 13:54:37 +0000

    TEAMANDO-4332 - add webpack caching (#145)

--- js/620.1666639180423.489d6f5629b66ee48de2.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksubscription_central=self.webpackChunksubscription_central||[]).push([[620],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var r=n(2950),a=n.n(r),c=n(334),o=n(6706),s=n(319),l=n(50),u=n(5240);const i=function(e){var t=e.logger;return a().createElement(o.zt,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},a().createElement(c.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},a().createElement(l.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/620.91f67c2f1ae5fb7e0279f6d908bc8868.js.map
